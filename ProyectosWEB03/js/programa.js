2b258494e78f32f62317b3f77ec46b7f7ef363c9
Josealvareza <49487363+Josealvareza@users.noreply.github.com>
2022-03-22 20:48:40 -0500

    Update programa.js

--- ProyectosWEB03/js/programa.js
+++ ProyectosWEB03/js/programa.js
@@ -13,3 +13,39 @@ $(document).ready(function(){
     });
   
   });
+
+$(document).ready(function(){
+    $("button").click(function(){
+        var valor = $("#numero4D").val();
+
+    var cantidadPares = 0;
+    var cantidadImpares = 0;
+    var cantidadCeros = 0;
+    var i = 0;
+    var numero = [4];
+
+    var numero4D =parseInt( document.getElementById('numero4D').value);
+    if (numero4D <= 1000){
+        alert("El numero no es de 4 digitos, intente de nuevo");
+    }else{
+         while(numero4D>0){
+             numero[i] = numero4D%10;
+             numero4D = parseInt(numero4D/10);
+
+             if(numero[i] == 0){
+                 cantidadCeros ++;
+            }else if(numero[i]%2 == 0){
+                cantidadPares ++;            
+            }else{
+                cantidadImpares ++;
+            }
+             i++;
+         }   
+    }
+
+    $("#pares").val(cantidadPares);
+    $("#impares").val(cantidadImpares);
+    $("#ceros").val(cantidadCeros);
+
+    });
+  });
